cdc8dee2e93be9e12019d489cffd39211e7f2983
SoccerDevC <cddavid2001@gmail.com>
2025-02-04 14:31:30 +0300

    finished classes and basic pdf reading, wrtting,  combining andlogging with exception handlinng and expression of some GUI

---
+++ .ipynb_checkpoints/my_modules-checkpoint.py
@@ -0,0 +1,5 @@
+data = {"Course":["robotics","data-science","blockchain"],"greeting":"greeting from ineuron"}
+def getData():
+    return data["Course"]
+def get_greeting():
+    return data["greeting"]
